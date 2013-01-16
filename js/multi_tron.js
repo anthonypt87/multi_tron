193720e5c9f332c8819a78346019957c95c45999
anthony <anthonypt87@gmail.com>
2013-01-16 01:38:21 -0800

    We got a dukey game

--- js/multi_tron.js
+++ js/multi_tron.js
@@ -9,4 +9,115 @@ var bgImage = new Image();
 bgImage.onload = function () {
 		bgReady = true;
 };
-bgImage.src = "images/background.png";
+bgImage.src = "images/background.jpg";
+
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function () {
+	heroReady = true;
+};
+heroImage.src = "images/hero.jpg";
+
+// Monster image
+var monsterReady = false;
+var monsterImage = new Image();
+monsterImage.onload = function () {
+	monsterReady = true;
+};
+monsterImage.src = "images/monster.gif";
+
+
+var hero = {
+		speed: 256, // movement in pixels per second
+			x: 0,
+				y: 0
+};
+var monster = {
+		x: 0,
+			y: 0
+};
+var monstersCaught = 0;
+
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+		keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+		delete keysDown[e.keyCode];
+}, false);
+
+// Update game objects
+function update (modifier) {
+	if (38 in keysDown) { // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		hero.x += hero.speed * modifier;
+	}
+
+	// Are they touching?
+	if (
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	) {
+		++monstersCaught;
+		reset();
+	}
+};
+
+
+// Reset the game when the player catches a monster
+function reset() {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+// Draw everything
+function render() {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+	}
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+	// Score
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+};
+
+function main() {
+	var now = Date.now();
+	var delta = now - then;
+
+	update(delta / 1000);
+	render();
+
+	then = now;
+};
+
+reset();
+var then = Date.now();
+setInterval(main, 1);
